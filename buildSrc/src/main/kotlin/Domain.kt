7f26282e7f12e519958d5da0990728ebc5556fdd
ali tafreshi <alitafreshi123@yahoo.com>
2022-03-09 10:43:13 +0330

    Done

--- buildSrc/src/main/kotlin/Domain.kt
+++ buildSrc/src/main/kotlin/Domain.kt
@@ -1,4 +1,4 @@
 object Domain {
-    private const val ayanCoreVersion = "0.0.13"
+    private const val ayanCoreVersion = "0.0.14"
     const val ayanCore = "com.github.alitafreshi:ayan-core:${ayanCoreVersion}"
 }
