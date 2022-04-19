943e1adf94060317cdf117a18da724b88f172a4c
ali tafreshi <alitafreshi123@yahoo.com>
2022-04-19 17:27:33 +0430

    updating bottom sheets for multi direction

--- compose-commom-ui/src/main/java/ir/tafreshiali/compose_commom_ui/bottom_sheet/ErrorBottomSheet.kt
+++ compose-commom-ui/src/main/java/ir/tafreshiali/compose_commom_ui/bottom_sheet/ErrorBottomSheet.kt
@@ -5,14 +5,19 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import ir.tafreshiali.compose_commom_ui.R
 import ir.tafreshiali.compose_commom_ui.responsiveness.spacing
 import ir.tafreshiali.compose_commom_ui.util.noRippleClickable
@@ -41,7 +46,6 @@ fun ErrorBottomSheet(
     errorButtonTextStyle: TextStyle = MaterialTheme.typography.button,
     backGroundColor: Color = Color.White,
     contentPadding: Dp = MaterialTheme.spacing.default,
-    contentVerticalArrangement: Arrangement.HorizontalOrVertical = Arrangement.Center,
     onButtonClick: () -> Unit,
     content: @Composable ((
         errorTitle: String,
@@ -79,7 +83,6 @@ fun ErrorBottomSheet(
             errorButtonTextStyle = errorButtonTextStyle,
             backGroundColor = backGroundColor,
             contentPadding = contentPadding,
-            contentVerticalArrangement = contentVerticalArrangement,
             onButtonClick = onButtonClick
         )
     }
@@ -97,11 +100,12 @@ fun ErrorBottomSheet(
  * @param errorDescriptionTextStyle the style of description of type [TextStyle]
  * @param errorButton the text of bottom sheet button
  * @param errorButtonTextStyle the style of bottom sheet button of type [TextStyle]
- * @param [backGroundColor] [contentPadding] [contentVerticalArrangement] are the bottom sheet properties
+ * @param [backGroundColor] [contentPadding] are the bottom sheet properties
  * @param onButtonClick a lambda function for reacting to the user clicks of bottom sheet button
  */
 
 
+/*
 @Composable
 fun ErrorBottomSheetContent(
     modifier: Modifier = Modifier,
@@ -152,4 +156,91 @@ fun ErrorBottomSheetContent(
                 .align(Alignment.End)
         )
     }
+}*/
+
+
+@Composable
+fun ErrorBottomSheetContent(
+    modifier: Modifier = Modifier,
+    errorTitle: String,
+    errorTitleTextStyle: TextStyle = MaterialTheme.typography.h4,
+    errorDescription: String,
+    errorDescriptionTextStyle: TextStyle = MaterialTheme.typography.subtitle2,
+    errorButton: String = stringResource(id = R.string.btn_retry),
+    errorButtonTextStyle: TextStyle = MaterialTheme.typography.button,
+    backGroundColor: Color = Color.White,
+    contentPadding: Dp = MaterialTheme.spacing.default,
+    onButtonClick: () -> Unit
+) {
+
+
+    ConstraintLayout(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .background(backGroundColor)
+            .padding(contentPadding)
+    ) {
+        val (tvError, tvErrorDescription, btnError) = createRefs()
+
+        Text(
+            text = errorTitle,
+            style = errorTitleTextStyle,
+            modifier = modifier
+                .constrainAs(tvError) {
+                    top.linkTo(parent.top)
+                    end.linkTo(parent.end)
+                    start.linkTo(parent.start)
+                    width = Dimension.fillToConstraints
+                }
+        )
+
+
+        Text(
+            text = errorDescription,
+            style = errorDescriptionTextStyle,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Right,
+            modifier = modifier
+                .constrainAs(tvErrorDescription) {
+                    top.linkTo(tvError.bottom)
+                    end.linkTo(parent.end)
+                    start.linkTo(parent.start)
+                    width = Dimension.fillToConstraints
+                }
+                .padding(vertical = MaterialTheme.spacing.extraSmall),
+        )
+
+        Text(
+            text = errorButton,
+            style = errorButtonTextStyle,
+            color = MaterialTheme.colors.primary,
+            fontWeight = FontWeight.Bold,
+            modifier = modifier
+                .constrainAs(btnError) {
+                    top.linkTo(tvErrorDescription.bottom)
+                    end.linkTo(parent.end)
+                    start.linkTo(parent.start)
+                    width = Dimension.fillToConstraints
+                }
+                .noRippleClickable(onClick = onButtonClick)
+        )
+    }
 }
+
+
+@Preview(showBackground = true)
+@Composable
+fun ErrorBottomSheetContentPreview(
+
+) {
+    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
+        ErrorBottomSheetContent(
+            errorTitle = "error",
+            errorDescription = "bad error happend",
+            errorButton = "retry"
+        ) {}
+    }
+}
+
+
--- compose-commom-ui/src/main/java/ir/tafreshiali/compose_commom_ui/bottom_sheet/InfoBottomSheet.kt
+++ compose-commom-ui/src/main/java/ir/tafreshiali/compose_commom_ui/bottom_sheet/InfoBottomSheet.kt
@@ -5,14 +5,20 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import ir.tafreshiali.compose_commom_ui.R
 import ir.tafreshiali.compose_commom_ui.responsiveness.spacing
 import ir.tafreshiali.compose_commom_ui.util.noRippleClickable
@@ -41,7 +47,6 @@ fun InfoBottomSheet(
     infoButtonTextStyle: TextStyle = MaterialTheme.typography.button,
     backGroundColor: Color = Color.White,
     contentPadding: Dp = MaterialTheme.spacing.large,
-    contentVerticalArrangement: Arrangement.HorizontalOrVertical = Arrangement.Center,
     onButtonClick: () -> Unit,
     content: @Composable ((
         infoTitle: String,
@@ -78,7 +83,6 @@ fun InfoBottomSheet(
             infoButtonTextStyle = infoButtonTextStyle,
             backGroundColor = backGroundColor,
             contentPadding = contentPadding,
-            contentVerticalArrangement = contentVerticalArrangement,
             onButtonClick = onButtonClick
         )
 
@@ -86,6 +90,7 @@ fun InfoBottomSheet(
 }
 
 
+/*
 @Composable
 fun InfoBottomSheetContent(
     modifier: Modifier = Modifier,
@@ -137,4 +142,93 @@ fun InfoBottomSheetContent(
                 .align(Alignment.End)
         )
     }
+}*/
+
+
+
+
+@Composable
+fun InfoBottomSheetContent(
+    modifier: Modifier = Modifier,
+    infoTitle: String = stringResource(id = R.string.tv_info),
+    infoTitleTextStyle: TextStyle = MaterialTheme.typography.h4,
+    infoDescription: String,
+    infoDescriptionTextStyle: TextStyle = MaterialTheme.typography.subtitle2,
+    infoButton: String = stringResource(id = R.string.btn_ok),
+    infoButtonTextStyle: TextStyle = MaterialTheme.typography.button,
+    backGroundColor: Color = Color.White,
+    contentPadding: Dp = MaterialTheme.spacing.default,
+    onButtonClick: () -> Unit
+) {
+
+    ConstraintLayout(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .background(backGroundColor)
+            .padding(contentPadding)
+    ) {
+        val (tvInfoTitle, tvInfoDescription, btnInfo) = createRefs()
+
+        Text(
+            text = infoTitle,
+            style = infoTitleTextStyle,
+            modifier = modifier
+                .fillMaxWidth()
+                .constrainAs(tvInfoTitle) {
+                    top.linkTo(parent.top)
+                    end.linkTo(parent.end)
+                    start.linkTo(parent.start)
+                    width = Dimension.fillToConstraints
+                }
+        )
+
+        Text(
+            text = infoDescription,
+            style = infoDescriptionTextStyle,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Right,
+            modifier = modifier
+                .constrainAs(tvInfoDescription) {
+                    top.linkTo(tvInfoTitle.bottom)
+                    end.linkTo(parent.end)
+                    start.linkTo(parent.start)
+                    width = Dimension.fillToConstraints
+                }
+                .padding(vertical = MaterialTheme.spacing.extraSmall),
+        )
+
+        Text(
+            text = infoButton,
+            style = infoButtonTextStyle,
+            color = MaterialTheme.colors.primary,
+            fontWeight = FontWeight.Bold,
+            modifier = modifier
+                .constrainAs(btnInfo) {
+                    top.linkTo(tvInfoDescription.bottom)
+                    end.linkTo(parent.end)
+                    start.linkTo(parent.start)
+                    width = Dimension.fillToConstraints
+                }
+                .noRippleClickable(onClick = onButtonClick)
+        )
+    }
 }
+
+
+@Preview(showBackground = true)
+@Composable
+fun InfoBottomSheetContentPreview() {
+
+    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr)
+    {
+        InfoBottomSheetContent(
+            infoDescription = "jkdfhgkdf",
+            infoTitle = "sdgvfsdg",
+            infoButton = "sdGVSD"
+        ) {
+
+        }
+    }
+}
+
